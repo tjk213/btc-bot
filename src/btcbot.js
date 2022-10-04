5ff863f8578683999c081f70a642171097b77e1d
Tyler Kenney <tjk213@gmail.com>
2022-10-03 20:22:47 -0400

    Remove default args on entrypoints
      - Default args do not appear to work when invoked from time-based
        triggers in google apps script.

--- src/btcbot.js
+++ src/btcbot.js
@@ -391,19 +391,19 @@ function appendEntry(ss = SpreadsheetApp.getActive(), intervalTimeInDays=1)
   return parseFloat(priceAction.lastPrice);
 }
 
-function appendDailyEntry(ss = SpreadsheetApp.getActive()) {
-  appendEntry(ss,1);
+function appendDailyEntry() {
+  return appendEntry(SpreadsheetApp.getActive(),1);
 }
 
-function appendWeeklyEntry(ss = SpreadsheetApp.getActive()) {
-  appendEntry(ss,7);
+function appendWeeklyEntry() {
+  return appendEntry(SpreadsheetApp.getActive(),7);
 }
 
 // Update BTC history & send summary email.
 function appendEntryAndEmailResults()
 {
   var ss = SpreadsheetApp.getActive();
-  var btcPrice = appendDailyEntry(ss);
+  var btcPrice = appendDailyEntry();
   emailResults(ss,btcPrice);
 }
 
